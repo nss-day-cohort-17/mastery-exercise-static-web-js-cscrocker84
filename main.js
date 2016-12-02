5d35c4190778077bd4dea3877db3feadb24acc0f
Charlie Crocker <cscrocker84@gmail.com>
2016-12-02 10:54:24 -0600

    first few commands

---
+++ main.js
@@ -0,0 +1,8 @@
+var button = document.querySelector("button");
+var input1 = document.querySelectorAll("input")[0];
+var input2 = document.querySelectorAll("input")[1];
+
+var treeDets = {
+    "height" : 0;
+    "buildChar" : 0;
+}
