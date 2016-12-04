20e18ddbb990345e572e80b5d120636dcbe5b88e
Charlie Crocker <cscrocker84@gmail.com>
2016-12-04 12:37:39 -0600

    added build tree function

--- main.js
+++ main.js
@@ -10,17 +10,32 @@ function checkInput () {
     }
     else {
         // It accepts a single object as an argument. The object should have two key/value pairs.
-        var treeDetails = {
+        var treeDets = {
         // A key that specifies the height of the pine tree.
             height:"";
         // A key that specifies which character to use to build the pine tree.
             char:"";
         }
         // 1. The value for the height of the tree should be from user input in a `<input type="text">` field in the DOM.
-        treeObj.height = document.getElementById("height").value;
+        treeDets.height = document.getElementById("height").value;
         // 1. The character to use should be from user input in a `<input type="text">` field in the DOM.
-        treeObj.char = document.getElementById("character").value;
+        treeDets.char = document.getElementById("character").value;
 
-        tree(treeObj);
+        tree(treeDets);
     }
+}
+
+// Create a `tree` function that should build a pine tree out of a character in the Chrome dev tools console.
+function tree (treeDets) {
+  for (var i = 0; i < treeDets.height; i++) {
+    console.log(' '.repeat((treeDets.height-1)-i) + treeDets.character.repeat(2*i + 1) + '\n');
+  }
+}
+
+function enterCheck (keyboardEvent) {
+  event.preventDefault();
+  console.log(keyboardEvent)
+  if (keyboardEvent.key === "Enter") {
+    checkInput();
+  }
 }
