ddb8e4729a1db98aaf58c8bade7dfc4ec8181883
Charlie Crocker <cscrocker84@gmail.com>
2016-12-04 18:05:10 -0600

    added eventListener for button and return key

--- main.js
+++ main.js
@@ -12,8 +12,8 @@ function checkInput () {
         // It accepts a single object as an argument. The object should have two key/value pairs.
         var treeDet = {
         // A key that specifies the height of the pine tree.
+          // A key that specifies which character to use to build the pine tree.
             height:"",
-        // A key that specifies which character to use to build the pine tree.
             char:""
         }
         // 1. The value for the height of the tree should be from user input in a `<input type="text">` field in the DOM.
@@ -24,7 +24,6 @@ function checkInput () {
         tree(treeDet);
     }
 }
-
 // Create a `tree` function that should build a pine tree out of a character in the Chrome dev tools console.
 function tree (treeDet) {
   for (var i = 0; i < treeDet.height; i++) {
@@ -39,3 +38,14 @@ function enterCheck (keyboardEvent) {
     checkInput();
   }
 }
+// Once the user enters in a number, and a character, the user can either then just
+//press the enter key _(as long as the cursor is in one of the input fields)_, or click
+//a button that is labeled "Grow your tree" and the tree should be shown in the console.
+//This requires you to add an event listener to the button, as well as an event listener for the enter/return key.
+
+var button = document.getElementById("button");
+button.addEventListener('click', checkInput);
+var heightEnter = document.getElementById("height");
+var charEnter = document.getElementById("char");
+heightEnter.addEventListener('keyup', enterCheck);
+charEnter.addEventListener('keyup', enterCheck);
